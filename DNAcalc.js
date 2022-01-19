205f253d8205da47169dbc36bbd2758c066b724c
Neoteus <29826035+Neoteus@users.noreply.github.com>
2022-01-19 13:41:57 +0800

    new file:   .gitignore 	new file:   DNAcalc.css 	new file:   DNAcalc.js 	new file:   clipboard.min.js

---
+++ DNAcalc.js
@@ -0,0 +1,22 @@
+function calComplementarySequence() {
+    const pairing = {
+        "A": "T", "T": "A", "G": "C", "C": "G",
+        "a": "t", "t": "a", "g": "c", "c": "g"
+    };
+    let legal_input = true;
+    let seq = document.getElementById("complementary-sequence-input").value;
+    let res = seq.split("").reverse();
+    for (let i = 0; i < res.length; i++) {
+        res[i] = pairing[res[i]];
+        if (!res[i]) {
+            legal_input = false;
+            break;
+        }
+    }
+    if (legal_input) {
+        res = res.join("");
+    } else {
+        res = "只允许输入ATGC（大小写皆可）"
+    }
+    document.getElementById("complementary-sequence-output").value = res;
+}
